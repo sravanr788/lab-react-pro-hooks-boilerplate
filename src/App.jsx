f776dad2d6329a4252e8930a91002bbdde59218b
sravanr788 <sravanr7882gmail.com>
2023-12-19 15:55:47 +0530

    prohooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React,{useEffect,useState} from 'react';
+import React,{useCallback, useEffect,useMemo,useState} from 'react';
 import './App.css';
 
 // Do not change this
@@ -15,17 +15,22 @@ function App() {
 
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const delayFunction = ()=> {
+  // const delayFunction = ()=> {
+  //   console.log("Delay Function Ran")
+  //   for(let index=0; index<LARGE_NUMBER; index++){};
+  //   return value+2;
+  // }
+
+  const delayFunction = useMemo(()=>{
     console.log("Delay Function Ran")
-    for(let index=0; index<LARGE_NUMBER; index++){};
-    return value+2;
-  
-  }
+      for(let index=0; index<LARGE_NUMBER; index++){};
+      return value+2;
+  },[value])
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const testFunction = ()=>{
+  const testFunction = useCallback(()=>{
     return [value*3 ,value*4]
-  }
+  },[value])
 
   
 
@@ -69,7 +74,7 @@ function App() {
       <h1 >{value}</h1>
       <button onClick={handleChangeValue}>Change Value</button>
       <button onClick={handleList}>Show List</button>
-      <h2>{delayFunction()}</h2>
+      <h2>{delayFunction}</h2>
       <div>
         {currentList.map((item,index)=>{
           return <h2 key={index}>{item}</h2>
